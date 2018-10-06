cddc6024f9a3d1a2931e42133f30900b7f9c13b3
ymm1x <biztk28@gmail.com>
2018-10-06 21:09:32 +0900

    Add license

--- src/Resource/function/shorthands.php
+++ src/Resource/function/shorthands.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the Ymm1x/XDump package.
+ *
+ * Copyright (c) 2018 ymm1x
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 if (!function_exists('d')) {
     function d(...$args): void
