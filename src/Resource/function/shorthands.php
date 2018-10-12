ed7ee13edbbcc868c11ab9693374abcfb0c79a01
ymm1x <biztk28@gmail.com>
2018-10-12 22:11:50 +0900

    Fix dump bug when there are multiple args

--- src/Resource/function/shorthands.php
+++ src/Resource/function/shorthands.php
@@ -9,14 +9,14 @@
  */
 
 if (!function_exists('d')) {
-    function d($args): void
+    function d(...$args): void
     {
-        \Ymm1x\XDump\Dumper::dump($args);
+        call_user_func_array('\Ymm1x\XDump\Dumper::dump', $args);
     }
 } else if (!function_exists('dd')) { // Retry declare
-    function dd($args): void
+    function dd(...$args): void
     {
-        \Ymm1x\XDump\Dumper::dump($args);
+        call_user_func_array('\Ymm1x\XDump\Dumper::dump', $args);
     }
 }
 
