3c4b6d0e015024982c358b749934e10988f1dd87
ymm1x <biztk28@gmail.com>
2018-10-12 21:04:33 +0900

    Fix extra array wrap

--- src/Resource/function/shorthands.php
+++ src/Resource/function/shorthands.php
@@ -9,12 +9,12 @@
  */
 
 if (!function_exists('d')) {
-    function d(...$args): void
+    function d($args): void
     {
         \Ymm1x\XDump\Dumper::dump($args);
     }
 } else if (!function_exists('dd')) { // Retry declare
-    function dd(...$args): void
+    function dd($args): void
     {
         \Ymm1x\XDump\Dumper::dump($args);
     }
